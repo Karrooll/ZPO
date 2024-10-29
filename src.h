418472326630f6ad44175b809ccd1c2f610537be
Karol <kkupis@student.agh.edu.pl>
2024-10-29 14:06:05 +0100

    Szkic metod i ich opisu

--- src.h
+++ src.h
@@ -7,22 +7,17 @@ from typing import Optional
 class Product:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
 
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-        # tutaj trzeba zadeklarować atrybty i dać odpowiednią walidacje
+    def __init__():
+
 
     def __eq__(self, other):
-        # metoda powinna porównywać 2 produkty(obiekty)
         return None  # FIXME: zwróć odpowiednią wartość
 
     def __hash__(self):
-        # to trzeba zmienić tak by działało
         return hash((self.name, self.price))
 
 
 class TooManyProductsFoundError:
-    # jak w instrukcji jest napisane że ma być rzucony włansy wyjątek to właśnie o ten chodzi
     # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
     pass
 
@@ -33,33 +28,15 @@ class TooManyProductsFoundError:
 #   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
 
 class ListServer:
-    def __init__(self, lista_obj):
-        self.lista_obj = lista_obj
-        # metoda który zaninicjalizuje serwer listowy
-
-    def add_product(self, product):
-        self.lista_obj.append(product) # to co tutaj wchodzi jako produkt to obiekt który chcemy dodać do serwera; listy
     pass
 
 
 class MapServer:
-    def __init__(self, dic_obj):
-        self.dic_obj = dic_obj
-    def add_product(self, product):
-        self.dic_obj[product.name] = product # to co tu wchodzi jako produkt to obiekt który chcemy dodać do serwera; słowinka
-        # metoda inicjalizująca serwer słownikowy
     pass
 
 
 class Client:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą obiekt reprezentujący serwer
-    def __init__(self, serwer):
-        self.serwer = serwer
-        # przy inicjalizacji podajemy serwer w którym szukamy produktów
-
-
-
 
     def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
-        # pobiera nazwy obiektów, wyszukuje w serwerze, zbiera cene
         raise NotImplementedError()
