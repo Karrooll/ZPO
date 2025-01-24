2066c56fe1cd049b6d6fef026a0b1afe9c26d175
Jan Majerczyk <167908274+Meloniaszbuk@users.noreply.github.com>
2025-01-24 13:37:12 +0100

    Create packageIO.hxx

---
+++ packageIO.hxx
@@ -0,0 +1,22 @@
+ifndef NETSIM_PACKAGE_HPP
+#define NETSIM_PACKAGE_HPP
+#include "types.hxx"
+#include <vector>
+
+class Package{
+public:
+    Package();
+    explicit Package(ElementID);
+    Package(Package&&) noexcept;
+    Package& operator = (Package&&) noexcept;
+    ElementID get_id() const {return ID_;};
+    ~Package();
+
+private:
+    static std::vector<ElementID> usedlist_;
+    static std::vector<ElementID> freelist_;
+    ElementID ID_;
+};
+
+
+#endif //NETSIM_PACKAGE_HPP
